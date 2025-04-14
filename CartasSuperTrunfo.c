e882af021a147b93bc2706b4352c9f6c58541de1
Warley-boop <sinister5432@gmail.com>
2025-04-14 20:40:34 +0000

    criei print e scanf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -25,4 +25,33 @@ int main() {
     printf ("Número de pontos turísticos: \n");
     scanf ("%d", &numero);
 
+
+
+
+    printf("Digite aqui as informações das suas cartas: \n");
+    
+    printf ("Carta 2 \n");
+    printf ("Código: \n");
+    scanf ("%d", &codigo);
+    
+    printf ("Nome: \n");
+    scanf ("%s", &nome);
+    
+    printf ("População: \n");
+    scanf ("%d", &populacao);
+    
+    printf ("Área: \n");
+    scanf ("%d", &area);
+    
+    printf ("PIB: \n");
+    scanf ("%d", &pib);
+    
+    printf ("Número de pontos turísticos: \n");
+    scanf ("%d", &numero);
+
+
+
+
+
+
 }

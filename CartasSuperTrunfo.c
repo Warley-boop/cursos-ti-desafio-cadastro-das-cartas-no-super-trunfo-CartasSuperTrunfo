10467a54b130c9a0aa7eeefb4b2647deffe82448
Warley-boop <sinister5432@gmail.com>
2025-04-15 00:45:12 +0000

    criei comparacao entre numeros float de nivel cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,60 +1,15 @@
 #include <stdio.h>
 
 int main() {
-float habitantes1, habitantes2, area1, area2, pib1, pib2;
-float densidade; 
-float ppc1, ppc2;
 
-printf("Carta 1: \n")
+   
+float carta1 = 1900000;
+float carta2 = 2000000;
 
-printf ("Digite a população: \n");
-scanf ("%D", &habitantes1);
-
-printf ("Digite a área: \n");
-scanf ("%D", &area1);
-
-densidade = (habitantes1 / area1);
-printf ("A densidade populacional da carta 1 é: %d", densidade);
-
-
-
-printf("Carta 2: \n")
-
-printf ("Digite a população: \n");
-scanf ("%d", &habitantes2);
-
-printf ("Digite a área: \n");
-scanf ("%d", &area2);
-
-densidade = (habitantes2 / area2);
-printf ("A densidade populacional da carta 2 é: %d", densidade);
-
-
-
-printf("Carta 1: \n")
-
-printf ("Digite a população: \n");
-scanf ("%d", &pib1);
-
-printf ("Digite a área: \n");
-scanf ("%d", &habitantes1);
-
-ppc1 = (pib1 / habitantes1);
-printf ("O PIB per Capita da carta 1 é: %d", ppc1);
-
-
-
-printf("Carta 2: \n")
-
-printf ("Digite a população: \n");
-scanf ("%d", &pib2);
-
-printf ("Digite a área: \n");
-scanf ("%d", &habitantes2);
-
-ppc2 = (pib2 / habitantes2);
-printf ("O PIB per Capita da carta 2 é: %d", ppc2);
-
-
+    
+    
+    printf("carta1 >= carta2: %d\n", carta1 >= carta2);
+    printf("carta1 == carta2: %d\n", carta1 == carta2);
+    printf("carta1 != carta2: %d\n", carta1 != carta2);
 
 }
